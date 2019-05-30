447004b492ae71f53d99123c5dc3b6425daedf75
Bogomaz, Oleg <oleg.bogomaz@ceridian.com>
2019-05-30 16:08:14 -0400

    Add spam service.

--- MLService/MLService/Program.cs
+++ MLService/MLService/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace oleg_ml_service
+namespace MLService
 {
     public class Program
     {
--- MLService/MLService/Startup.cs
+++ MLService/MLService/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace oleg_ml_service
+namespace MLService
 {
     public class Startup
     {
