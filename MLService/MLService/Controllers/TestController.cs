54bf2446745707e2b7ca88a9043e557370da24f4
Bogomaz, Oleg <oleg.bogomaz@ceridian.com>
2019-05-31 12:16:31 -0400

    Update launch entrypoint.

---
+++ MLService/MLService/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "We are up!";
+        }
+    }
+}
